c848026ca3dc8c1611beb63eba4ab3de9d298dab
Iris Pecquet <iris.pecquet@lacatholille.fr>
2024-03-18 15:51:47 +0100

    Menu update

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -7,32 +9,50 @@ namespace Managers
 {
     public class UIManager : Singleton<UIManager>
     {
+        [Header("Slider Menu")]
         [SerializeField] private GameObject _menuObj;
-        [SerializeField] private GameObject _levelMenuObj;
         [SerializeField] private Slider _mainMenuSlider;
+        [SerializeField] private Image _imageSlider;
+        
+        [Space(8)]
+        [SerializeField] private GameObject _levelMenuObj;
 
         protected override void InternalAwake()
         {
         }
-        
-        public void OpenLevelMenu()
+
+        private void Start()
         {
-            if (_mainMenuSlider.value >= 0.9f)
+            if (PlayerPrefs.GetInt("FirstPlay") == 1)
             {
                 _menuObj.SetActive(false);
                 _levelMenuObj.SetActive(true);
             }
         }
 
+        public void OpenLevelMenu()
+        {
+            float value = _mainMenuSlider.value;
+
+            if (value <= 0.1f)
+            {
+                _menuObj.SetActive(false);
+                _levelMenuObj.SetActive(true);
+                
+                PlayerPrefs.SetInt("FirstPlay", 1);
+            }
+
+            _imageSlider.DOFade(value, 0.1f);
+        }
+
         public IEnumerator GoToScene(string nameScene)
         {
             // mets ta transi ici
-            
+
             float seconds = 0; // mets le temps de ton anim
             yield return new WaitForSeconds(seconds);
-            
+
             SceneManager.LoadScene(nameScene);
         }
-
     }
 }
--- Assets/Scripts/UI/LevelButton.cs
+++ Assets/Scripts/UI/LevelButton.cs
@@ -1,5 +1,6 @@
 using System;
 using Managers;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,16 +11,20 @@ namespace UI
         [SerializeField] private int _dimensionIndex;
         [SerializeField] private Image[] _starsImages;
         [SerializeField] private Sprite _starFullSprite;
+        [SerializeField] private TMP_Text _levelName;
 
         private Button _button;
         
         private void Start()
         {
+            string scene = $"Dimension{_dimensionIndex}";
+            
             _button = GetComponent<Button>();
-            _button.onClick.AddListener(() => StartCoroutine(UIManager.Instance.GoToScene($"Dimension{_dimensionIndex}")));
+            _button.onClick.AddListener(() => StartCoroutine(UIManager.Instance.GoToScene(scene)));
 
-            int stars = PlayerPrefs.GetInt($"Dimension{_dimensionIndex}");
-
+            _levelName.text = $"Dimension {_dimensionIndex}";
+            
+            int stars = PlayerPrefs.GetInt(scene);
             for (int i = 0; i < stars; i++)
             {
                 _starsImages[i].sprite = _starFullSprite;
