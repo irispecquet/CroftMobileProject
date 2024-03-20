d07946cc149c305e2a29a3f4befba1d5f4815832
Iris Pecquet <iris.pecquet@lacatholille.fr>
2024-03-20 10:02:26 +0100

    Scroll prefab and script (lock)

---
+++ Assets/Scripts/UI/ScrollViewLevels.cs
@@ -0,0 +1,41 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class ScrollViewLevels : MonoBehaviour
+    {
+        [SerializeField] private int _starsMin;
+        [SerializeField] private GameObject _padlockObject;
+        [SerializeField] private TMP_Text _starsMinText;
+        [SerializeField] private LevelButton[] _levels;
+
+        private void Start()
+        {
+            if (_starsMin > 0)
+            {
+                _starsMinText.text = _starsMin.ToString();
+
+                bool canBeSelected = PlayerPrefs.GetInt("TotalStars") >= _starsMin;
+                
+                Lock(!canBeSelected);
+            }
+            else
+            {
+                Lock(false);
+            }
+        }
+
+        private void Lock(bool isLocked)
+        {
+            _padlockObject.gameObject.SetActive(isLocked);
+            _starsMinText.gameObject.SetActive(isLocked);
+
+            foreach (LevelButton level in _levels)
+            {
+                level.CanBeSelected = !isLocked;
+            }
+        }
+    }
+}
